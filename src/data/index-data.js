efa3a2ab1e80371b4ac50b34dfb0a71f7f1f32d3
Rafael Ramos <rramo012@gmail.com>
2022-07-06 17:24:43 -0400

    Fix path when proxied

--- src/data/index-data.js
+++ src/data/index-data.js
@@ -3,7 +3,7 @@ export default {
 		{
 			title: "Cloud Operators Manual - Zero to Production in 40 Hours",
 			description: "Key document for all IT teams that are managing their own Private Cloud.",
-			htmlPath: "./manuals/operators-manual/",
+			htmlPath: "/docs/manuals/operators-manual/",
 			pdfPath: "https://inmotionhosting.github.io/static-assets/docs/openstack_ops_manual.pdf",
 			comingSoon: false,
 			manualName: 'Operator\'s Manual',
@@ -14,7 +14,7 @@ export default {
 		{
 			title: "Cloud Operators Manual - Extras",
 			description: "Extra guides an OpenStack administrator may find interesting.",
-			htmlPath: "./manuals/operators-manual-extras/",
+			htmlPath: "/docs/manuals/operators-manual-extras/",
 			pdfPath: "",
 			comingSoon: false,
 			manualName: 'Operator\'s Manual Extras',
@@ -42,7 +42,7 @@ export default {
 		{
 			title: "OpenStack Horizon Quick Start for Project Users",
 			description: "Helps new Project Users to self serve their Infrastructure.",
-			htmlPath: "./manuals/users-manual/",
+			htmlPath: "/docs/manuals/users-manual/",
 			pdfPath: "",
 			comingSoon: false,
 			manualName: 'User\'s Manual',
@@ -53,7 +53,7 @@ export default {
 		{
 			title: "Cloud Operators Manual Extended - Kubernetes on OpenStack",
 			description: "Learn how to use your OpenStack to run your Kubernetes deployments.",
-			htmlPath: "./manuals/operators-manual-extras/magnum-and-kubernetes",
+			htmlPath: "/docs/manuals/operators-manual-extras/magnum-and-kubernetes",
 			pdfPath: "",
 			comingSoon: false,
 			catagories: ['users'],
