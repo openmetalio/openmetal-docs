91bef05ddb6702ceadf8c0b261abb0365d1e92b4
Rafael Ramos <rramo012@gmail.com>
2022-10-13 13:32:35 -0400

    Remove url updating

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,11 +54,6 @@ const config = {
       <%~ metaAttribute %>
     <% }); %>
     <%~ it.headTags %>
-	<script>
-		if (window && window.location && window.location.pathname.endsWith('/') && window.location.pathname !== '/') {
-			window.history.replaceState('', '', window.location.pathname.substr(0, window.location.pathname.length - 1))
-		}
-	</script>
     <% it.stylesheets.forEach((stylesheet) => { %>
       <link rel="stylesheet" href="<%= it.baseUrl %><%= stylesheet %>" />
     <% }); %>
